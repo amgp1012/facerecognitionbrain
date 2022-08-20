9ba74c46ddb379a0fc2e22b72f78dba63cccb9ad
amgp1012 <alvaro.m.guevara@gmail.com>
2022-08-20 16:55:23 -0700

    added clarifai API

--- src/App.js
+++ src/App.js
@@ -3,22 +3,40 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Clarifai from 'clarifai';
 import './App.css';
+
+const app = new Clarifai.App({
+  apiKey: '58768eb2ecca4bb9962647ea290c6459'
+});
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      input: ''
+      input: '',
+      imageUrl: ''
     };
   }
 
   onNewInput = (event) => {
-    console.log(event.target.value);
+    this.setState({input: event.target.value});
   }
 
   onButtonSubmit = () => {
-    console.log('click');
+    this.setState({imageURL: this.state.input});
+    app.models.predict(
+      Clarifai.FACE_DETECT_MODEL,
+      this.state.input)
+    .then(
+      function(response) {
+        console.log(response);
+      },
+      function(err) {
+
+      }
+    );
   }
   
   render() {
@@ -31,7 +49,7 @@ class App extends Component {
           onNewInput={this.onNewInput}
           onButtonSubmit={this.onButtonSubmit} 
         />
-        {/*<FaceRecognition />*/}
+        <FaceRecognition imageUrl={this.state.imageURL} />
       </div>
     );
   }
