cc9959e79f5ba024d3a8171cb0f4f67ec870c366
amgp1012 <alvaro.m.guevara@gmail.com>
2022-10-09 13:06:38 -0700

    heroku deployment changes

--- src/App.js
+++ src/App.js
@@ -62,7 +62,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    fetch('http://localhost:3000/imageurl', {
+    fetch('https://lit-ocean-55851.herokuapp.com/imageurl', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -72,7 +72,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://lit-ocean-55851.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
